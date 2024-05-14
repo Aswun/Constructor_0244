272480788d3062c2dd9ebf220cef4e965b988cf2
Aswun <aswinlutfian32@gmail.com>
2024-05-14 15:24:18 +0700

    melanjutkan class mahasiswa

--- Constructor.cpp
+++ Constructor.cpp
@@ -8,4 +8,15 @@ public:
     int nim;
     string nama;
 
+public:
+    mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+
+    mahasiswa(int iNim)
+    {
+        nim = iNim;
+    }
 };
