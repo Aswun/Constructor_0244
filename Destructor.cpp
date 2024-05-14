25874b227b5a8b5e188e1d5aa47ee4c458ce44c7
Aswun <aswinlutfian32@gmail.com>
2024-05-14 16:50:39 +0700

    menambahkah int main

--- Destructor.cpp
+++ Destructor.cpp
@@ -45,4 +45,14 @@ void angka::isiData()
         cout << i << " = ";
         cin >> arr[i];
     }
+    cout << endl;
+}
+
+int main()
+{
+    angka belajarcpp(3);
+    angka *ptrBelajarcpp = new angka(5);
+    delete ptrBelajarcpp;
+
+    return 0;
 }
